8f82a728a53e7c338014a6eb7e7276d138eb3d95
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:57:31 +0200

    Update split 1 .js
    Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,7 +1,8 @@
 // Split an array into two parts
 // Step 1: Renamed Function and Variables
-let splitArray = (splitIndex, array) => {
-  begin = array.slice(0, splitIndex);
+// Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.
+const splitArray = (splitIndex, array) => {
+  const begin = array.slice(0, splitIndex);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
