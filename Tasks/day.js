ec5e90f4f03a796228c5fed294f3004cd206d3c5
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 17:30:02 +0200

    Update day 1 .js
    Step 2: Used const instead of let for the daysOfWeek array and add const for numDay

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,9 @@
 // Get day number
 // Step 1: Renamed the function _parse_day_ to numDay. Changed variable name D to daysOfWeek and changed name variable s to dayString
-let daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// Step 2: Used const instead of let for the daysOfWeek array and add const for numDay
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-numDay = (dayString) => {
+const numDay = (dayString) => {
   let i;
   for (i = 0; i < daysOfWeek.length; i++) {
     if (dayString.startsWith(daysOfWeek[i].toLowerCase())) {
