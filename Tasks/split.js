bedcaa74527167b850aa4c66411d4dc6dbfa7c4e
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:55:41 +0200

    Update split 0 .js

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,10 +1,10 @@
 // Split an array into two parts
-
-let split_Array = (index, array) => {
-  begin = array.slice(0, index);
+// Step 1: Renamed Function and Variables
+let splitArray = (splitIndex, array) => {
+  begin = array.slice(0, splitIndex);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
 };
 
-module.exports = split_Array;
+module.exports = splitArray;
