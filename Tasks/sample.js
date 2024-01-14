a310757e98abbdefeb75abcd57bf177e7eea1285
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:45:24 +0200

    Update sample 1 .js
    Step 2 renamed varieble arr to a new const variable randomIndex to store the randomly generated index value

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,7 +1,8 @@
 // Get one random element from an array
 //Step 1: Renamed the function from SAMPLE to getRandomElement
+//Step 2 renamed varieble arr to a new const variable randomIndex to store the randomly generated index value
 const getRandomElement = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
+  const randomIndex = arr[Math.floor(Math.random() * arr.length)];
   return arr;
 };
 
