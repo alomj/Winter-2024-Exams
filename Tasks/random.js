ad05f763c4dea932410442e6f7295a1751d9c1fe
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:05:30 +0200

    Update random Step 2.js
    Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,6 +1,8 @@
 // Generate random integer value in given range
 //Step 1: Renamed the function from Random to getRandom 
-getRandom = (min, max) => {
+//Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.
+
+const getRandom = (min, max) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
