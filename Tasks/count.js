e1075971d01ed1db2da5957fb4c86f3675805279
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 19:14:42 +0200

    Update count 2.js
    step 3: Restiling

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,15 @@
 // Sum all number values in dict
 // Step: 1  adding const`s and let
 // Step 2: replacing forEach to For of
+// Step 3: Restiling
 const count = (obj) => {
 let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
   for (const key of keys){
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
-  ({});
+  }
   return sum;
-};
+}
 
 module.exports = count;
