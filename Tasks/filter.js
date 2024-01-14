a149cc15ae53f36fe8720454243409db62cf56bc
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 19:28:10 +0200

    Update filter 1 .js
    Renaming variables

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,16 @@
 // Filter array by type name
 //Step 1: Renaming function
-filterByTyoe = (T, t) => {
+//Step 2: Renaming variables
+filterByType = (array, typeName) => {
   remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
+  for (C of array) {
+    x = array.indexOf(C);
+    if (typeof array[x] !== typeName) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (x of remove) array.splice(x, 1);
+  return array;
 };
 
-module.exports = Filter;
+module.exports = filterByType;
