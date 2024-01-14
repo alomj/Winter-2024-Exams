2b28c33b8ac0b65c76df4e22e8b489824115b50e
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 17:14:51 +0200

    Update split 2.js
    Step 3: I`ve deleted useless line for better code readability

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,10 +1,10 @@
 // Split an array into two parts
 // Step 1: Renamed Function and Variables
 // Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.
+// Step 3: I`ve deleted useless line for better code readability
 const splitArray = (splitIndex, array) => {
   const begin = array.slice(0, splitIndex);
-  const len = array.length;
-  array = array.slice(index, len);
+  array = array.slice(splitIndex, array.length);
   return [begin, array];
 };
 
