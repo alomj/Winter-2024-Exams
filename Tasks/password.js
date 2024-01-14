b19c838afb71c659bc6e074b5f4725723f9dbd4c
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:26:04 +0200

    Update password Step:2.js
    Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,6 +1,7 @@
 // Generate random password
 //Step 1: Renamed function from GeneratePaswword to generatePassword. Renamed variables
-let generatePassword = (alphabet, length) => {
+//Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.
+const generatePassword = (alphabet, length) => {
   const maxPassLen = alphabet.length;
   pasw = '';
   for (let i = 0; i < length; i++) {
