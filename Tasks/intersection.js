9d5e6df438152ef0ed6a06c7452e6921ed903231
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 20:10:09 +0200

    Update intersection 2.js
    Making code more readable

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,24 +1,16 @@
 // Find an intersection of two dictionaries
 // Step 1: Renaming variables
 // Step 2: I have added const to my function and "element"
+// Step 3: Making code more readable
 const intersection = function intersection(firstDictionary, secondDictionary) {
   keys = Object.keys(firstDictionary);
-  {
-  }
+  
   for (const element of keys) {
     if (firstDictionary[element] === secondDictionary[element]) {
-     secodnDictionary[element] = firstDictionary[element];
-      {
-      }
+     secondDictionary[element] = firstDictionary[element];
     } else {
       delete firstDictionary[element];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return firstDictionary;
 };
