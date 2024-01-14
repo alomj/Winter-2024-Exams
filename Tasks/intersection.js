4203a0177ae7b7bc283d83f33b8041b951f72727
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 20:12:21 +0200

    Update intersection 3.js
    Step 4: I`ve used  not-equal-to operator to provide cleaner code

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,15 +2,15 @@
 // Step 1: Renaming variables
 // Step 2: I have added const to my function and "element"
 // Step 3: Making code more readable
+// Step 4: I`ve used  not-equal-to operator to provide cleaner code
 const intersection = function intersection(firstDictionary, secondDictionary) {
   keys = Object.keys(firstDictionary);
   
   for (const element of keys) {
-    if (firstDictionary[element] === secondDictionary[element]) {
-     secondDictionary[element] = firstDictionary[element];
-    } else {
-      delete firstDictionary[element];
+    if (firstDictionary[element] !== secondDictionary[element]) {
+       delete firstDictionary[element];
     }
+    
   }
   return firstDictionary;
 };
