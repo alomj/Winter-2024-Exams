db988560ac0281eef753cc0c06ce141f4a6a488c
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 19:51:02 +0200

    Update filter 3 .js
    I`ve made code more optimized and readable and fixed few misstakes

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,15 +2,23 @@
 //Step 1: Renaming function
 //Step 2: Renaming variables
 //Step 3: Adding const`s and let
+//Step 4: i`ve made code more optimized and readable
 const filterByType = (array, typeName) => {
   let remove = [];
+
+
   for (const element of array) {
-    x = array.indexOf(C);
-    if (typeof array[x] !== typeName) {
-      remove.unshift(x);
+    if (typeof element !== typeName) {
+      const index  = array.indexOf(element);
+      remove.unshift(index);
     }
   }
-  for (x of remove) array.splice(x, 1);
+
+  for (const element of remove){
+    array.splice(element, 1);
+
+  }
+
   return array;
 };
 
