e689acf38efcedb846e108f12a3ee2cafc1c910a
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 18:49:24 +0200

    Update between 1.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,10 +1,11 @@
 // Extract substring between prefix and suffix
 // Step 1: Renaming variables
-getValuebetween = (fullString, prefix, suffix) => {
-  index = fullString.indexOf(prefix);
+// Step 2: Adding const`s and let
+const getValuebetween = (fullString, prefix, suffix) => {
+const index = fullString.indexOf(prefix);
   if (index === -1) return '';
   else {
-    prefixLength = index + prefix.length;
+  let prefixLength = index + prefix.length;
     fullString = fullString.substring(prefixLengh);
     if (suffix) {
       index = fullString.indexOf(suffix);
