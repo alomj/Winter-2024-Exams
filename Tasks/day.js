8f37d3d2875d22c31c0d0f400338c0b6d847a24e
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 17:31:48 +0200

    Update day 2.js
    Step 3: Removed the unnecessary variable i declaration outside the loop and initialized it directly in the loop statement.

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 // Step 1: Renamed the function _parse_day_ to numDay. Changed variable name D to daysOfWeek and changed name variable s to dayString
 // Step 2: Used const instead of let for the daysOfWeek array and add const for numDay
+// Step 3: Removed the unnecessary variable i declaration outside the loop and initialized it directly in the loop statement.
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const numDay = (dayString) => {
-  let i;
-  for (i = 0; i < daysOfWeek.length; i++) {
+  for (let i = 0; i < daysOfWeek.length; i++) {
     if (dayString.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
