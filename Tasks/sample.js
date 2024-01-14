b5f0cb3bc2c4f4448965de98ca8d228c7e2e9417
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:52:04 +0200

    Update sample 4.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -2,10 +2,11 @@
 //Step 1: Renamed the function from SAMPLE to getRandomElement
 //Step 2: Renamed variable arr to a new const variable randomIndex to store the randomly generated index value
 //Step 3 Created a new variable randomElement to store the randomly selected element from the array using the randomIndex.
+//Step 4: Returned the randomElement instead of the arr variable.
 const getRandomElement = (arr) => {
   const randomIndex = [Math.floor(Math.random() * arr.length)];
   const randomElement = arr[randomIndex];
-  return arr;
+  return randomElement;
 };
 
 module.exports = getRandomElement;
