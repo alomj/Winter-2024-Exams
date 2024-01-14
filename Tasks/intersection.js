f3922e30a01f44992ec34cdd797c97883700a480
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 20:03:01 +0200

    Update intersection 1 .js
    Step 2: I have added const to my function and "element"

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,10 +1,11 @@
 // Find an intersection of two dictionaries
 // Step 1: Renaming variables
-intersection = function intersection(firstDictionary, secondDictionary) {
+// Step 2: I have added const to my function and "element"
+const intersection = function intersection(firstDictionary, secondDictionary) {
   keys = Object.keys(firstDictionary);
   {
   }
-  for (element of keys) {
+  for (const element of keys) {
     if (firstDictionary[element] === secondDictionary[element]) {
      secodnDictionary[element] = firstDictionary[element];
       {
