53e38b2a621738a9ba251fb28b746bf4925962dd
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 18:18:46 +0200

    Update ip 2 .js
    Step 3: restiling code and make him more readable

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,17 +1,24 @@
 // Split string by the first occurrence of separator
 // Step 1: renamed variables
 // Step 2: added const`s and let
+// Step 3: restiling code and make him more readable
 const parseip = (input) => {
- let array = [];
-  if (input === ''){ return;
+const array = [];
+  if (input === ''){ 
+     return;
   }  else {
   const arrayOfIpEl = input.split('.');
-    if (arrayOfIpEl.length != 4){return;};
-    j = 0;
-    for (const b of B) {
-      arrayj] = parseInt(b);
-      if (isNaN(array[j])) return;
-      j++;
+    if (arrayOfIpEl.length !== 4){
+       return;
+    }
+    let index = 0;
+    for (const segment of arrayOfIpEl) {
+      array[index] = parseInt(segment);
+      if (isNaN(array[index])){
+       return;
+      }
+     
+      index++;
     }
   }
   return array;
