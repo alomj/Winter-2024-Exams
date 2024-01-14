fd5a16ee1056d34d1df2bb1f4868d70657fe134b
alomj <144105248+alomj@users.noreply.github.com>
2024-01-14 16:29:17 +0200

    Update password Step:3.js
    Step 3: Used addition assignment for better code readability

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,12 +1,13 @@
 // Generate random password
 //Step 1: Renamed function from GeneratePaswword to generatePassword. Renamed variables
 //Step 2: Used const instead of let for the function declaration to indicate that the function reference will not be reassigned.
+//Step 3: Used addition assignment for better code readability
 const generatePassword = (alphabet, length) => {
   const maxPassLen = alphabet.length;
   pasw = '';
   for (let i = 0; i < length; i++) {
     Index = Math.floor(Math.random() * maxPassLen);
-    pasw = pasw + alphabet[Index];
+    pasw += alphabet[Index];
   }
   return pasw;
 };
